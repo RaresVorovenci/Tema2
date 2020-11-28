18c909cc7cee329e1b97b07a07c7f17d1a623dbd
Vorovenci_Rares <vorovencirares18@stud.ase.ro>
2020-11-28 22:27:12 +0200

    Tema2

--- main/app.js
+++ main/app.js
@@ -1,7 +1,23 @@
 function addTokens(input, tokens){
-    
-}
+  
+    if(!(typeof input==='string'))
+   throw "Invalid input";
 
+  if(input.length<6)
+   throw "Input should have at least 6 characters";
+
+ 
+
+   for (let i = 0; i < tokens.length; i++) {
+      if(!(typeof tokens[i].tokenName==='string'))
+     throw "Invalid array format";}
+ 
+if(!(input.includes('...')))
+   return input;
+else
+  input=input.replace('...',"${subsemnatul}");
+  return input;
+}
 const app = {
     addTokens: addTokens
 }
